b06d9177e8e55ec413f5b349a15d4893f1334275
Tomasz Trzaskoma <przytrzask@gmail.com>
2020-09-26 01:54:04 +0200

    image loader

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   target: "serverless",
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.module.rules.push({
+      test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/, /\.svg$/],
+      loader: require.resolve("url-loader"),
+    })
+
+    return config
+  },
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,85 +1,94 @@
 /** @jsx jsx */
+import React from "react"
 import { jsx } from "theme-ui"
 import Head from "next/head"
 import { Flex } from "theme-ui"
+import Confetti from "react-confetti"
 
-const Home = () => (
-  <div className="container">
-    <Head>
-      <title>Create Next App</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+// @ts-ignore
+import image from "../Image.svg"
 
-    <main sx={styles.main}>
-      <Flex sx={styles.history} bg="primary">
-        <img src="/image.svg" alt="ZEIT Logo" />
-      </Flex>
-      <Flex
-        style={{ flexDirection: "column" }}
-        p={4}
-        sx={styles.game}
-        bg="muted"
-      >
-        <h1>Get In</h1>
-        <p>Witaj w nowym najlepszym panelu klienta jaki widział świat.</p>
-        <label>
-          <input type="text" />
-        </label>
-        <label>
-          <input type="password" />
-        </label>
-        <button>Zaloguj</button>
-        <p>
-          <small>
-            Dokładnie sprawdzaj, czy informacje zawarte w SMSie autoryzacyjnym
-            są zgodne z danymi transakcji
-          </small>
-        </p>
-      </Flex>
-    </main>
-
-    <style jsx>{`
-      img {
-        height: 100%;
-        width: auto;
-      }
-      label {
-        width: 320px;
-        padding-bottom: 2rem;
-      }
-      input {
-        width: 320px;
-        height: 44px;
-        left: 0px;
-        top: 0px;
-        background: #ffffff;
-        border: 1px solid #1a2151;
-        border-radius: 10px;
-      }
-      button {
-        display: flex;
-        flex-direction: column;
-        align-items: flex-start;
-        width: 320px;
-        height: 43px;
-        left: 0px;
-        top: 0px;
-        background: #00cffd;
-        border-radius: 22px;
-        justify-content: center;
-        align-items: center;
-      }
-      .container {
-        min-height: 100vh;
-        background: rgba(0, 0, 0, 0.01);
-        backdrop-filter: blur(250px);
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-    `}</style>
-  </div>
-)
+const Home = () => {
+  const [showConfetti, setShowConfetti] = React.useState(false)
+  return (
+    <div className="container">
+      {showConfetti && <Confetti />}
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main sx={styles.main}>
+        <Flex sx={styles.history} bg="primary">
+          <img src={image} alt="ZEIT Logo" />
+        </Flex>
+        <Flex
+          style={{ flexDirection: "column" }}
+          p={4}
+          sx={styles.game}
+          bg="muted"
+        >
+          <h1>Get In</h1>
+          <p>Witaj w nowym najlepszym panelu klienta jaki widział świat.</p>
+          <label>
+            <input type="text" />
+          </label>
+          <label>
+            <input type="password" />
+          </label>
+          <button onClick={() => setShowConfetti((prev) => !prev)}>
+            Zaloguj
+          </button>
+          <p>
+            <small>
+              Dokładnie sprawdzaj, czy informacje zawarte w SMSie autoryzacyjnym
+              są zgodne z danymi transakcji
+            </small>
+          </p>
+        </Flex>
+      </main>
+      <style jsx>{`
+        img {
+          height: 100%;
+          width: auto;
+        }
+        label {
+          width: 320px;
+          padding-bottom: 2rem;
+        }
+        input {
+          width: 320px;
+          height: 44px;
+          left: 0px;
+          top: 0px;
+          background: #ffffff;
+          border: 1px solid #1a2151;
+          border-radius: 10px;
+        }
+        button {
+          display: flex;
+          flex-direction: column;
+          align-items: flex-start;
+          width: 320px;
+          height: 43px;
+          left: 0px;
+          top: 0px;
+          background: #00cffd;
+          border-radius: 22px;
+          justify-content: center;
+          align-items: center;
+        }
+        .container {
+          min-height: 100vh;
+          background: rgba(0, 0, 0, 0.01);
+          backdrop-filter: blur(250px);
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+      `}</style>
+    </div>
+  )
+}
 
 const styles = {
   header: {
